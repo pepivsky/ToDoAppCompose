8d05a95192d56b4ba2d45f5db2d5a8ec187a6014
pepivsky <ironglas68@gmail.com>
2022-12-28 11:38:40 -0600

    feat: created taskContent in taskScreen

--- app/src/main/java/com/pepivsky/todocompose/components/PriorityDropDown.kt
+++ app/src/main/java/com/pepivsky/todocompose/components/PriorityDropDown.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,14 +47,15 @@ fun PriorityDropDown(
             .height(PRIORITY_DROP_DOWN_HEIGHT)
             .border(
                 width = 1.dp,
-                color = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.disabled)
+                color = MaterialTheme.colors.onSurface.copy(alpha = ContentAlpha.disabled),
+                shape = MaterialTheme.shapes.small
             )
-            .padding(all = SMALL_PADDING)
             .clickable {
                 expanded = true
             },
         verticalAlignment = Alignment.CenterVertically
     ) {
+        Spacer(modifier = Modifier.size(SMALL_PADDING))
         Box(
             modifier = Modifier
                 .size(PRIORITY_INDICATOR_SIZE)
@@ -81,7 +83,7 @@ fun PriorityDropDown(
             )
         }
         DropdownMenu(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth(fraction = 0.95F),
             expanded = expanded,
             onDismissRequest = { expanded = false }) {
             // low
--- app/src/main/java/com/pepivsky/todocompose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/pepivsky/todocompose/ui/screens/task/TaskScreen.kt
@@ -2,6 +2,7 @@ package com.pepivsky.todocompose.ui.screens.task
 
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import com.pepivsky.todocompose.data.models.Priority
 import com.pepivsky.todocompose.data.models.ToDoTask
 import com.pepivsky.todocompose.util.Action
 
@@ -14,7 +15,14 @@ fun TaskScreen(
         topBar = {
             TaskAppBar(navigateToListScreen = navigateToListScreen, selectedTask = selectedTask)
         }, content = {
-
+            TaskContent(
+                title = "",
+                onTitleChange = {},
+                description = "",
+                onDescriptionChange = {},
+                priority = Priority.HIGH,
+                onPrioritySelected = {}
+            )
         }
     )
 
