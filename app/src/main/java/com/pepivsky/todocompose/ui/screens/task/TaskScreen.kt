f93b6aad7fcdcfdf7250c9a5344a7468434cb206
pepivsky <ironglas68@gmail.com>
2023-01-23 20:37:15 -0600

    fix: added function to intercept back button

--- app/src/main/java/com/pepivsky/todocompose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/pepivsky/todocompose/ui/screens/task/TaskScreen.kt
@@ -1,11 +1,14 @@
 package com.pepivsky.todocompose.ui.screens.task
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
+import androidx.activity.addCallback
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
 import com.pepivsky.todocompose.data.models.Priority
 import com.pepivsky.todocompose.data.models.ToDoTask
@@ -25,6 +28,9 @@ fun TaskScreen(
     val priority by sharedViewModel.priority
 
     val context = LocalContext.current
+
+    // for handle when back button is pressed
+    BackHandler(onBackPressed = { navigateToListScreen(Action.NO_ACTION) })
 
     Scaffold(
         topBar = {
@@ -63,4 +69,32 @@ fun TaskScreen(
 
 fun displayToast(context: Context) =
     Toast.makeText(context, "Fields empty", Toast.LENGTH_SHORT).show()
+
+// intercepta el boton back del sistema, de este modo se evita un bug
+@Composable
+fun BackHandler(
+    backDispatcher: OnBackPressedDispatcher? =
+        LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher,
+    onBackPressed: () -> Unit
+) {
+    val currentOnBackPressed by rememberUpdatedState(newValue = onBackPressed)
+
+    val backCallBack = remember {
+        object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                currentOnBackPressed()
+                Log.d("BACK HANDLER", "Triggered")
+            }
+
+        }
+    }
+
+    DisposableEffect(key1 = backDispatcher) {
+        backDispatcher?.addCallback(backCallBack)
+
+        onDispose {
+            backCallBack.remove()
+        }
+    }
+}
 
